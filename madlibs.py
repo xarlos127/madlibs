a3cbfecc2536300d2051b915bd1c318660ef74e3
Carlos H <carlos.hernandez@csedge.org>
2024-06-25 15:24:41 -0700

    Madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,13 @@
 # MadLib by Carlos
 
 print("Here is my first commit :)")
+
+name = input("Name") 
+he_or_she = input("he Or She")
+hisorher = input ("his or her")
+age = input ("age")
+verb_ending_in_ing = input ("verb ending in ing")
+adjective = input("adjective")
+band_name = input("Band_Name")
+
+print(f"Once upon a time, there was a talented musician named {name}. {he_or_she} had been playing the guitar since {age} and had a passion for music that burned brightly in {hisorher} heart. One day, while {verb_ending_in_ing} in the park, {name} was approached by a {adjective} stranger who turned out to be a talent scout for the world-famous band {band_name} . The scout had been {verb_ending_in_ing} and had heard {name}'s incredible guitar skills from afar. Without hesitation, the scout offered {name} the opportunity of a lifetime: to join {band_name} as their lead guitarist on their upcoming world tour. Overwhelmed with excitement, {name} accepted the offer and embarked on a musical journey that would change {hisorher} life forever.")
